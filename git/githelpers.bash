c140f5120492e2c6d386852592a470c5294598f5
Nikhil Damle <nikhilbdamle@gmail.com>
2014-07-17 09:03:43 +0530

    git l to show pretty git log

---
+++ git/githelpers.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function pretty_git_log() {
+  local HASH="%C(yellow)%h%C(reset)"
+  local RELATIVE_TIME="%C(green)%ar%C(reset)"
+  local AUTHOR="%C(blue)%an%C(reset)"
+  local REFS="%C(red)%d%C(reset)"
+  local SUBJECT="%s"
+
+  local FORMAT="$HASH-$REFS $SUBJECT-$RELATIVE_TIME-$AUTHOR"
+
+  git log --graph --color --decorate --pretty="tformat:$FORMAT" $* |
+  # Looks like column from util-linux package has a bug.
+  #column -t -s '}' |
+  less -FXRS
+}
+
+export -f pretty_git_log
