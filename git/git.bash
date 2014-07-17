3e7e13bb29326ba3e19217eabad9a252342f74fe
Nikhil Damle <nikhilbdamle@gmail.com>
2014-07-17 10:57:43 +0530

    Source bash_completion on ubuntu if file exist which add completion for git and other few commands

--- git/git.bash
+++ git/git.bash
@@ -4,3 +4,5 @@ source_if_exist source /usr/share/git/completion/git-completion.bash
 # displays unstaged (*) and staged (+) changes will be shown next to the branch name in the __git_ps1 prompt
 export GIT_PS1_SHOWDIRTYSTATE=1
 source_if_exist /usr/share/git/completion/git-prompt.sh
+# Ubuntu completion
+source_if_exist /etc/bash_completion
