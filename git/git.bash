1836f66bed6284d769e1eaeed0e023f0b320e2a5
Nikhil Damle <nikhilbdamle@gmail.com>
2014-07-17 11:45:39 +0530

    Revert sourcing ubuntu completion

--- git/git.bash
+++ git/git.bash
@@ -4,5 +4,3 @@ source_if_exist source /usr/share/git/completion/git-completion.bash
 # displays unstaged (*) and staged (+) changes will be shown next to the branch name in the __git_ps1 prompt
 export GIT_PS1_SHOWDIRTYSTATE=1
 source_if_exist /usr/share/git/completion/git-prompt.sh
-# Ubuntu completion
-source_if_exist /etc/bash_completion
